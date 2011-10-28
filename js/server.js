1cfd38dcfec1132e4f52f3d6ed04ad1e2c456ee0
randomsilver <Vivienne.McGregor@lbi.com>
2011-10-28 13:26:41 +0100

    Added file containing assets required by node.js server i.e.css and JavaScript files and index.html file

---
+++ js/server.js
@@ -0,0 +1,17 @@
+HOST = null; // localhost
+PORT = 8001;
+
+var fu = require("./fu"),
+    sys = require("sys"),
+    url = require("url"),
+    qs = require("querystring");
+
+fu.listen(PORT, HOST);
+
+fu.get("/index.html", fu.staticHandler("index.html"));
+fu.get("/css/style.css", fu.staticHandler("css/style.css"));
+fu.get("/js/processing.js", fu.staticHandler("js/processing.js"));
+fu.get("/js/jsOAuth-1.3.1.js", fu.staticHandler("js/jsOAuth-1.3.1.js"));
+
+
+
